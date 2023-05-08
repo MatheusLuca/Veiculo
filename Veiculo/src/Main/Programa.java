294d8da3b486fcb72aebc612d711a8b1eb647f9c
matheus.barragao <matheus.barragao@ABRACAM018.ABRACAM.LOCAL>
2023-05-08 17:48:48 -0300

    classe random para gerar tempo aleatório.

--- Veiculo/src/Main/Programa.java
+++ Veiculo/src/Main/Programa.java
@@ -1,19 +1,23 @@
 package Main;
+
 import java.util.Locale;
 import java.util.Scanner;
 import java.util.Random;
 import classes.Carro;
+
+
 
 public class Programa {
 
 	public static void main(String[] args) {
 		Scanner  input = new Scanner(System.in);
 			
-		//Variaveis para criação de um veiculo
+		//Variaveis para inputs
 		String marca, modelo, placa, tipoDeCombustivel;
-		int ano;
+		int ano, capacidadeTanque = 55 ;
 		double combustivel;
 		
+		//inputs do usuario
 		System.out.println("----VEICULOS----");
 		System.out.println("Qual a marca?");
 		marca = input.next();
@@ -28,23 +32,40 @@ public class Programa {
 		System.out.println("Tipo de combustivel");
 		tipoDeCombustivel = input.next();
 		
-		//variaveis para o carro
+		//variaveis para o calculo carro
 		double distanciaPercorrida;
 		double quantidadeDeCombustivel;
 		System.out.println("Para calcular o consumo de combustivel\n");
-		System.out.println("Insira o valor do hodômetro km");
+		System.out.println("Insira o valor da distancia percorrida em km");
 		distanciaPercorrida = input.nextDouble();
 		System.out.println("Quantidade de combustivel marcado litro?");
 		quantidadeDeCombustivel = input.nextDouble();
-		Carro carro1 = new Carro(marca, modelo, placa, ano, combustivel, tipoDeCombustivel, distanciaPercorrida, quantidadeDeCombustivel);
 		
+		
+		
+		Carro carro1 = new Carro(marca, modelo, placa, ano, combustivel, tipoDeCombustivel, distanciaPercorrida, quantidadeDeCombustivel, capacidadeTanque);
+		
+		//calculo de consumo de combustivel
 		double consumoFinalDeCombustivel;
 		consumoFinalDeCombustivel = carro1.consumirCombustivel(distanciaPercorrida, quantidadeDeCombustivel);
 		System.out.printf("Consumo sobre as informações passadas é de %.2f%n", consumoFinalDeCombustivel);
-
 		
-		Random random = new Random();
-		double tempo = random.nextDouble(10);
+		
+		
+		
+		// gerar tempo aleatório para calculo de velocidade media.
+		
+		double tempo = 0;
+		double tempoValido = 0;
+		Random gerarTempoRandomico = new Random();
+		
+				tempo = gerarTempoRandomico.nextDouble();
+				tempoValido = tempo;	
+			
+		System.out.println("Insira o tempo da viagem:" + tempoValido);
+		System.out.println("Velocidade média em km/h: " + carro1.velocidadeMedia(distanciaPercorrida, tempo));
+		
+		
 		
 	}
 
