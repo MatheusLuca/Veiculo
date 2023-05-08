daae5dc09f09d144d5def04f03d0c4fa7901890c
matheus.barragao <matheus.barragao@ABRACAM018.ABRACAM.LOCAL>
2023-05-08 16:15:05 -0300

    Invocação do metodo calcula consumo medio combustivel

--- Veiculo/src/Main/Programa.java
+++ Veiculo/src/Main/Programa.java
@@ -1,16 +1,14 @@
 package Main;
 import java.util.Locale;
 import java.util.Scanner;
-
+import java.util.Random;
 import classes.Carro;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		Scanner  input = new Scanner(System.in);
-		
-		
-		
+			
 		//Variaveis para criação de um veiculo
 		String marca, modelo, placa, tipoDeCombustivel;
 		int ano;
@@ -34,15 +32,19 @@ public class Programa {
 		double distanciaPercorrida;
 		double quantidadeDeCombustivel;
 		System.out.println("Para calcular o consumo de combustivel\n");
-		System.out.println("Insira a distancia percoridda");
+		System.out.println("Insira o valor do hodômetro km");
 		distanciaPercorrida = input.nextDouble();
-		System.out.println("Quantidade de combustivel usado?");
+		System.out.println("Quantidade de combustivel marcado litro?");
 		quantidadeDeCombustivel = input.nextDouble();
-		
 		Carro carro1 = new Carro(marca, modelo, placa, ano, combustivel, tipoDeCombustivel, distanciaPercorrida, quantidadeDeCombustivel);
 		
+		double consumoFinalDeCombustivel;
+		consumoFinalDeCombustivel = carro1.consumirCombustivel(distanciaPercorrida, quantidadeDeCombustivel);
+		System.out.printf("Consumo sobre as informações passadas é de %.2f%n", consumoFinalDeCombustivel);
+
 		
-		System.out.println(carro1.consumirCombustivel(distanciaPercorrida, quantidadeDeCombustivel));
+		Random random = new Random();
+		double tempo = random.nextDouble(10);
 		
 	}
 
