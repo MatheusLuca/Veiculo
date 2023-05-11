e4396e63a9574394194b1bf65a1fcb3b05326a9b
matheus.barragao <matheus.barragao@ABRACAM018.ABRACAM.LOCAL>
2023-05-11 14:02:33 -0300

    Estrutura de controle para moto ou para carro

--- Veiculo/src/Main/Programa.java
+++ Veiculo/src/Main/Programa.java
@@ -4,6 +4,9 @@ import java.util.Locale;
 import java.util.Scanner;
 import java.util.Random;
 import classes.Carro;
+import classes.Moto;
+import classes.Veiculos;
+
 import java.lang.Math;
 
 
@@ -14,55 +17,96 @@ public class Programa {
 			
 		//Variaveis para inputs
 		String marca, modelo, placa, tipoDeCombustivel;
-		int ano, capacidadeTanque;
-		double combustivelVeiculo;
+		int ano;
+		int escolhaVeiculo;
 		
-		//inputs do usuario
-		System.out.println("----VEICULOS----");
-		System.out.println("Qual a marca?");
-		marca = input.next();
-		System.out.println("Modelo?");
-		modelo = input.next();
-		System.out.println("Placa do veiculo");
-		placa = input.next();
-		System.out.println("Qual o ano do veiculo?");
-		ano = input.nextInt();
-		System.out.println("Tipo de combustivel");
-		tipoDeCombustivel = input.next();
+		System.out.println("1 para carro ---- 2 para moto");
+		escolhaVeiculo = input.nextInt();
+		switch(escolhaVeiculo) {
+		case 1:
+			System.out.println("----CARRO----");
+			System.out.println("Qual a marca?");
+			marca = input.next();
+			System.out.println("Modelo?");
+			modelo = input.next();
+			System.out.println("Placa do veiculo");
+			placa = input.next();
+			System.out.println("Qual o ano do veiculo?");
+			ano = input.nextInt();
+			System.out.println("Tipo de combustivel");
+			tipoDeCombustivel = input.next();
+				
+			Carro carro1 = new Carro(marca,  modelo,  placa,  ano,  tipoDeCombustivel);
+			double qtdCombustivelAtual = carro1.geraCombustivel();
 			
-		Carro carro1 = new Carro(marca,  modelo,  placa,  ano,  tipoDeCombustivel);
-		double qtdCombustivelAtual = carro1.geraCombustivel();
-		System.out.printf("\nO valor do combustivel atual do carro é %.2f\n" , qtdCombustivelAtual); 			
-	
-		System.out.println("\nInsira um valor de distancia percorrida por KM:");
-		double distanciaPercorrida = input.nextDouble();
-		double consumoPorKilometro = carro1.consumirCombustivel(distanciaPercorrida, qtdCombustivelAtual);
-		System.out.println("\nSeu carro fez " + consumoPorKilometro + " km" +" litro");
+			
+			System.out.printf("\n------O valor do combustivel atual do carro é %.2f\n------" , qtdCombustivelAtual); 			
 		
-		System.out.println("Voce ainda tem km" + carro1.avisaAutonomia(qtdCombustivelAtual, consumoPorKilometro));
-		
-			// gerar tempo aleatório para calculo de velocidade media.
+			System.out.println("\nInsira um valor de distancia percorrida por KM:");
+			double distanciaPercorrida = input.nextDouble();
+			double consumoPorKilometro = carro1.consumirCombustivel(distanciaPercorrida, qtdCombustivelAtual);
+			System.out.printf("O consumo de combustivel por quilometro é de : %.2f\n" , consumoPorKilometro);
 			
+			System.out.println(carro1.avisaAutonomia(qtdCombustivelAtual, consumoPorKilometro));
 			
-			
-			double tempo = 0;
-			double tempoValido = 0;
-			Random gerarTempoRandomico = new Random();
+				// gerar tempo aleatório para calculo de velocidade media.
+					double tempo = 0;
+					double tempoValido = 0;
+					Random gerarTempoRandomico = new Random();
 			
 					tempo = gerarTempoRandomico.nextDouble();
 					tempoValido = tempo / 60;			
 					Math.round(tempoValido);
+					System.out.printf("Tempo da viagem foi de %.2f hora\n " , tempoValido);
 					
 					
+					double velocidade = carro1.velocidadeMedia(distanciaPercorrida, tempoValido);
 					
-			System.out.println("Velocidade média em km/h: " + carro1.velocidadeMedia(distanciaPercorrida, tempo));
+				System.out.printf("Velocidade: %.2f\n", velocidade);
+				System.out.println(carro1.imprimeDadosVeiculo());
+			break;
+		case 2:
+				System.out.println("----CARRO----");
+				System.out.println("Qual a marca?");
+				marca = input.next();
+				System.out.println("Modelo?");
+				modelo = input.next();
+				System.out.println("Placa do veiculo");
+				placa = input.next();
+				System.out.println("Qual o ano do veiculo?");
+				ano = input.nextInt();
+				System.out.println("Tipo de combustivel");
+				tipoDeCombustivel = input.next();
+				
+					Moto moto1 = new Moto(marca, modelo, placa, ano, tipoDeCombustivel, ano);
+					double qtdCombustivelAtualMoto = moto1.geraCombustivel();
+					System.out.printf("\n------O valor do combustivel atual do carro é %.2f\n------" , qtdCombustivelAtualMoto); 			
+					
+					System.out.println("\nInsira um valor de distancia percorrida por KM:");
+					double distanciaMotoPercorrida = input.nextDouble();
+					double consumoMotoPorKM = moto1.consumirCombustivel(distanciaMotoPercorrida, qtdCombustivelAtualMoto);
+					System.out.printf("O consumo de combustivel por quilometro é de : %.2f\n" , consumoMotoPorKM);
+					
+					System.out.println(moto1.avisaAutonomia(qtdCombustivelAtualMoto, consumoMotoPorKM));
+					
+					double tempoMoto = 0;
+					double tempoValidoMoto = 0;
+					Random gerarTempoRandomicoMoto = new Random();
 			
-			
-			
+					tempoMoto = gerarTempoRandomicoMoto.nextDouble();
+					tempoValidoMoto = tempoMoto / 60;			
+					Math.round(tempoValidoMoto);
+					System.out.printf("Tempo da viagem foi de %.2f hora\n " , tempoValidoMoto);
+					
+					double velocidadeMoto = moto1.velocidadeMedia(distanciaMotoPercorrida, tempoValidoMoto);
+					
+					System.out.printf("Velocidade: %.2f\n", velocidadeMoto);
+					System.out.println(moto1.imprimeDadosVeiculo());
+					
+			break;
+		}
 		
 		
-	
-
 		}
 
 }
